9c97d4932606b0fb75b66f1a82a77475fb9c5017
Samira Esquina <samiraesquina@gmail.com>
2020-06-25 14:14:30 -0400

    Here is my commit message

---
+++ script.js
@@ -0,0 +1,10 @@
+/var start = prompt("Are You Ready to fight? yes/no");
+
+class USS  {
+    constructor(name) {
+        this.name = 'USS Schwarzennegger'; 
+        this.hull = 20; 
+        this.accuracy = .7;
+        this.firepower = 5;     
+        
+  
